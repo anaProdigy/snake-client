e8a7b8e30851643d3143262bbb2d2c8639ce1018
Anastasia <z.anastasia11@gmail.com>
2023-02-15 18:59:12 -0800

    add ctl+C to exit the game

--- play.js
+++ play.js
@@ -3,3 +3,26 @@ const connect = require("./client.js")
  
 console.log("Connecting ...");
 connect();
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") { // if user presses Ctrl+C
+    process.exit(); // exit the process
+  }
+};
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+
+
+  return stdin;
+};
+
+// call the setupInput function to enable the input interface
+setupInput();
