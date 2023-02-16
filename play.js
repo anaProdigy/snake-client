6bb81a31effb0f51e5161193d7a00102264ca8d8
Anastasia <z.anastasia11@gmail.com>
2023-02-15 19:08:49 -0800

    refactor by moving input function in input.js

--- play.js
+++ play.js
@@ -1,28 +1,11 @@
 const connect = require("./client.js")
 
+const setupInput = require("./input.js")
+
  
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function(key) {
-  if (key === "\u0003") { // if user presses Ctrl+C
-    process.exit(); // exit the process
-  }
-};
-// setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-
-
-  return stdin;
-};
 
 // call the setupInput function to enable the input interface
 setupInput();
